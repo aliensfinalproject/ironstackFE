8683ca64d7acc820f87d9edc25703f69f2b38ce6
Ashima Shrivastava <ashima206@gmail.com>
2016-11-28 22:11:30 -0500

    changed routes and views

--- src/js/app.core/routes.js
+++ src/js/app.core/routes.js
@@ -3,11 +3,11 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
    .state('root', {
      abstract: true,
      templateUrl: 'templates/layout.tpl.html',
-     controller: 'LayoutController as layout'
+     //controller: 'LayoutController as layout'
    })
-   .state('root.home', {
-     url: '/home',
-     templateUrl: 'templates/home.tpl.html',
+   .state('root.main', {
+     url: '/main',
+     templateUrl: 'templates/main.tpl.html',
     //  controller: 'NewsfeedController as newsfeed'
    })
    .state('root.register', {
@@ -15,10 +15,23 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
      templateUrl: 'templates/register.tpl.html',
      controller: 'RegisterController as register'
    })
-   .state('root.login', {
+   /*.state('root.login', {
      url: '/login',
      templateUrl: 'templates/login.tpl.html',
      controller: 'LoginController as login'
+   })*/
+
+   .state('user',{
+      abstract: true,
+      templateUrl: 'templates'
+   })
+   .state('user.home'{
+    url:'/home',
+    templateUrl: 'templates/home.tpl.html'
+   })
+   .state('user.class'{
+    url: '/class',
+    templateUrl: 'templates/class.tpl.html'
    })
 
  $urlRouterProvider.otherwise('/home');
