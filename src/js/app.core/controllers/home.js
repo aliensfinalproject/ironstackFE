a262cbc9fd3192d6575988212cc9deb34427512d
Ashima Shrivastava <ashima206@gmail.com>
2016-12-09 11:57:18 -0500

    fixed the back button on postdetails

--- src/js/app.core/controllers/home.js
+++ src/js/app.core/controllers/home.js
@@ -1,6 +1,7 @@
 function HomeController(UserService,$state,$rootScope){
 	let vm =this
 	vm.userposts = []
+
 	vm.displayPosts = function(){
 		UserService.userPost().then(
 			resp =>{
@@ -11,12 +12,18 @@ function HomeController(UserService,$state,$rootScope){
 	vm.displayPosts();
 
 	vm.removePost = function(class_id,post_id){
+		console.log(class_id)
 		UserService.deletePost(class_id,post_id).then(
+
 			resp =>{
 				console.log(resp)
 				vm.displayPosts();
 
 			})
+	}
+	vm.backToClass = function(class_id){
+		console.log('hi')
+		$state.go('root.user.class.about',{id:class_id})
 	}
 }
 
