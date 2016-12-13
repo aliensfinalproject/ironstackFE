1943efe231c5ed865bc26f85e8ca82cb86f3cc3f
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-13 09:42:52 -0500

    separated my posts page according to category to match the class about page

--- src/js/app.core/controllers/home.js
+++ src/js/app.core/controllers/home.js
@@ -1,11 +1,28 @@
 function HomeController(UserService,$state,$rootScope){
 	let vm =this
 	vm.userposts = [];
+	vm.questions = [];
+	vm.videos = [];
+	vm.codes = [];
+	vm.statuses = [];
 
 	vm.displayPosts = function(){
 		UserService.userPost().then(
 			resp =>{
 				vm.userposts = resp.data
+
+				vm.questions = vm.userposts.filter(function(post){
+					return post.category === 'question';
+				})
+				vm.videos = vm.userposts.filter(function(post){
+					return post.category === 'video';
+				})
+				vm.codes = vm.userposts.filter(function(post){
+					return post.category === 'code';
+				})
+				vm.statuses = vm.userposts.filter(function(post){
+					return post.category === 'status';
+				})
 			})
 	}
 	vm.displayPosts();
