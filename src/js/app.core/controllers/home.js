a5dcacd82cee1ba2ea06c94e24c912c492322529
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-13 11:19:16 -0500

    added moments code for created at to change the format of the visible date

--- src/js/app.core/controllers/home.js
+++ src/js/app.core/controllers/home.js
@@ -14,15 +14,31 @@ function HomeController(UserService,$state,$rootScope){
 				vm.questions = vm.userposts.filter(function(userpost){
 					return userpost.category === 'question';
 				})
+				for(let i=0; i<vm.questions.length; i++){
+					vm.questions[i].created_at = moment(vm.questions[i].created_at).format('MMMM Do YYYY')
+				}
+
 				vm.videos = vm.userposts.filter(function(userpost){
 					return userpost.category === 'video';
 				})
+				for(let i=0; i<vm.videos.length; i++){
+					vm.videos[i].created_at = moment(vm.videos[i].created_at).format('MMMM Do YYYY')
+				}
+
 				vm.codes = vm.userposts.filter(function(userpost){
 					return userpost.category === 'code';
 				})
+				for(let i=0; i<vm.codes.length; i++){
+					vm.codes[i].created_at = moment(vm.codes[i].created_at).format('MMMM Do YYYY')
+				}
+
 				vm.statuses = vm.userposts.filter(function(userpost){
 					return userpost.category === 'status';
 				})
+				for(let i=0; i<vm.statuses.length; i++){
+					vm.statuses[i].created_at = moment(vm.statuses[i].created_at).format('MMMM Do YYYY')
+				}
+
 			})
 	}
 	vm.displayPosts();
