83d4ae06788926639c60584d9fa812e48ea594f0
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-13 16:51:44 -0500

    merged my addingTime branch to master

--- src/js/app.core/controllers/class.js
+++ src/js/app.core/controllers/class.js
@@ -1,23 +1,28 @@
+import moment from 'moment';
+
 function ClassController(UserService,$state,$rootScope){
-	
+
 	 let vm = this;
 	 vm.list = [];
 	 vm.isAdmin= UserService.isAdmin();
-	 
-	
+
+
 
 	 vm.listclasses = function(){
 	 	UserService.getClasses().then(
 	 		resp =>{
-	 			
-	 			vm.list = resp.data;
 
-	 		})
+	 			vm.list = resp.data;
+				for(let i=0 ; i<vm.list.length;i++){
+				 vm.list[i].startDate = moment(vm.list[i].startDate).format('MMMM Do YYYY')
+
+	 		}
+		})
 	 }
 	 vm.listclasses();
-	 
 
-	
+
+
 
 }
 ClassController.$inject = ['UserService', '$state', '$rootScope'];
